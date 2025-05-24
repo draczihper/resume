8df78fe4e692fc461abbbb497baa28b2449062c3
Kelvin Mvungi <109474683+draczihper@users.noreply.github.com>
2025-05-24 10:39:29 +0000

    Render components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Profile from "./components/Profile";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Profile />
+    <Education />
+    <Experience />
+  </StrictMode>
+);
