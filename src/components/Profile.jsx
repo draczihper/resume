ea06f28e616168a15eefb84c1ac27d7907bda25c
Drac Zihper <draczihper@gmail.com>
2025-05-25 14:08:24 +0300

    Edit profile form and styles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,15 +2,13 @@ import { useState } from "react";
 
 export default function Profile() {
   return (
-    <div className="form">
-      <label>Name: </label>
-      <input type="text" />
-
-      <label>Email: </label>
-      <input type="text" />
-
-      <label>Phone: </label>
-      <input type="text" />
+    <div className="profile-details">
+      <label htmlFor="name">Name:</label>
+        <input type="text" name="name"/><br></br>
+     <label htmlFor="email">Email:</label>
+        <input type="text" name="email"/><br></br>
+     <label htmlFor="phone">Phone:</label>
+      <input type="text" name="phone" /><br></br>
     </div>
   );
 }
