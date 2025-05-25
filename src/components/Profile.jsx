fc859f783e0d28f0a445898129e8fbeedf03d814
Drac Zihper <draczihper@gmail.com>
2025-05-25 21:56:47 +0300

    Add input button and Profile components

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
+import { useState } from 'react';
+
+function Input({label, id}) {
+  const [value, setValue] = useState(' ');
+
+  function handleInput(e) {
+  setValue(e.target.value);
+  console.log(e.target.value)
+  }
+
+  return (
+    <label htmlFor={id}>{label}:
+      <input type="text" id={id} value={value} onInput={handleInput}/>
+    </label>
+  )
+}
+
+function Button({child}) {
+  return (
+    <button>{child}</button>
+  )
+}
+
 
 export default function Profile() {
+
   return (
     <div className="profile-details">
-      <label htmlFor="name">Name:</label>
-        <input type="text" name="name"/><br></br>
-     <label htmlFor="email">Email:</label>
-        <input type="text" name="email"/><br></br>
-     <label htmlFor="phone">Phone:</label>
-      <input type="text" name="phone" /><br></br>
+        <form action="">
+      <Input label={"Full Name"} id={"name"}/>
+      <Input label={"Email"} id={"email"}/>
+      <Input label={"Phone"} id={"phone"}/>
+      </form>
+      <Button child={"Edit"} />
+      <Button child={"Submit"} />
     </div>
-  );
+  )
 }
