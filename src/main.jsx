586b720ec1edb05817f5f78f206cec27ad64f0d0
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2025-05-30 23:20:54 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import './styles/styles.css';
-import Profile from "./components/Profile";
-// import Education from "./components/Education";
+import Profile from "./components/Profile";import Education from "./components/Education";
 // import Experience from "./components/Experience";
 
 createRoot(document.getElementById("root")).render(
