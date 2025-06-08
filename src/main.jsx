298d1a1944dd87f9b6bfb70da2e7c393b69b80b7
Drac Zihper <draczihper@gmail.com>
2025-06-08 22:37:59 +0300

    Remove rendered components

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import './styles/styles.css';
-import Profile from "./components/Profile";import Education from "./components/Education";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Profile />
-    <Education />
-    <Experience />
+   
   </StrictMode>
 );
