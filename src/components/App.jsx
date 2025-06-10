99b450406be5951588a7dbb9509db86fe2d5ff2c
Kelvin Mvungi <109474683+draczihper@users.noreply.github.com>
2025-06-10 08:39:47 +0000

    Add Input component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,23 @@
-function App(){
-
-
-    return (
-        <h1></h1>
-    )
+function App() {
+  return (
+    <>
+      <Input label="Name: " />
+      <Input label="Email: " />
+      <Input label="Phone: " />
+      <div>
+        <h1>CV</h1>
+        <p></p>
+      </div>
+    </>
+  );
 }
 export default App;
+
+const Input = ({ label }) => {
+  return (
+    <div>
+      <label htmlFor="name">{label}</label>
+      <input type="text" id="name" />
+    </div>
+  );
+};
