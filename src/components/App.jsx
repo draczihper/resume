a705463d3db88bc8646970320313cbec7a1aa6fd
Kelvin Mvungi <109474683+draczihper@users.noreply.github.com>
2025-06-10 09:10:09 +0000

    Add event handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
 function App() {
+  const [name, setName] = useState("John Doe");
+  const [email, setEmail] = useState("johndoe@email.com");
+  const [phone, setPhone] = useState(0);
+
   return (
     <>
-      <Input label="Name: " />
-      <Input label="Email: " />
-      <Input label="Phone: " />
+      <Input
+        label="Name: "
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        label="Email: "
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        label="Phone: "
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
       <div>
         <h1>CV</h1>
-        <p></p>
+        <p>My name is {name}.</p>
+        <p>
+          You can contact me at {email} call me on {phone}
+        </p>
       </div>
     </>
   );
 }
 export default App;
 
-const Input = ({ label }) => {
+const Input = ({ label, value, onChange }) => {
   return (
     <div>
       <label htmlFor="name">{label}</label>
-      <input type="text" id="name" />
+      <input type="text" id="name" value={value} onChange={onChange} />
     </div>
   );
 };
