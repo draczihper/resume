70dc04beb21c903229e79fc718f117d2810ffaaf
Kelvin Mvungi <109474683+draczihper@users.noreply.github.com>
2025-06-11 15:01:39 +0000

    Change intial form state structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 function App() {
-  const [form, setForm] = useState({
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-    school: "Blue Sky university",
-    programme: "Astronomy",
-    year: "1850-55",
-    company: "Canopus",
-    title: "Planetary Astronomer",
-    responsibilities: "Search for extra terrestial habitable planets",
-  });
+  const [form, setForm] = useState([
+    { name: "John Doe" },
+    { email: "johndoe@example.com" },
+    { phone: "+1234567890" },
+    { school: "Blue Sky university", programme: "Astronomy" },
+    { year: "1850-55" },
+    { company: "Canopus" },
+    { title: "Planetary Astronomer" },
+    { responsibilities: "Search for extra terrestial habitable planets" },
+  ]);
 
   const handleChange = (e) => {
     setForm({
