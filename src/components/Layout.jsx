40cc9c8b52c228b6ecbbeaeab64f2dc07a5d15dd
Drac Zihper <draczihper@gmail.com>
2025-06-18 22:27:46 +0300

    Add layout for the cv

---
+++ src/components/Layout.jsx
@@ -0,0 +1,25 @@
+import * as form from './Form'
+
+
+function Layout(){
+    return (
+        <>
+        <div>
+        <h1>CV</h1>
+        <p>My name is {form.name}.</p>
+        <p>
+          You can contact me at {form.email} call me on {form.phone}.
+        </p>
+        <p>
+          I studied my Master's of {form.programme} at {form.school} between the
+          years of {form.year}.
+        </p>
+        <p>
+          I gained valuable knowledge and expertise that allowed me to work at{" "}
+          {form.company} as a {form.title}
+        </p>
+      </div>
+        </>
+    )
+}
+export default Layout;
