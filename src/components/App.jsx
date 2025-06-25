55b9abbca73a406144a1f946ee7930efc254a39a
Drac Zihper <draczihper@gmail.com>
2025-06-25 20:20:10 +0300

    Add form object and lift state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
-import Form from './Form';
 import Layout from './Layout';
 import Button from './Button';
+import Form from './Form';
+import { useState } from 'react';
 
 
 function App() {  
+  const [form, setForm] = useState({
+    name: "John Doe",
+    email: "johndoe@example.com",
+    phone: "+1234567890",
+    school: "Blue Sky university",
+    programme: "Astronomy",
+    year: "1850-55",
+    company: "Canopus",
+    title: "Planetary Astronomer",
+    responsibilities: "search for extra terrestial habitable planets",
+  })
   return (
     <>
-    <Form />
+    <Form form={form} setForm={setForm} />
     <Button child="Submit"/>
-    <Layout />
+    <Layout form={form} />
     </>
   )
 }
