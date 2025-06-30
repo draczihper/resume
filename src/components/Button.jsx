4f7f2fbaa81bdaba68af339c62a055d66c9a6a35
Drac Zihper <draczihper@gmail.com>
2025-06-30 19:09:10 +0300

    Style the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
  const Button = ({label, type = "button", onClick}) => {
-  return <button type={type} onClick={onClick}>{label}</button>;
+  return <button type={type} onClick={onClick} className="bg-sky-900 w-full mt-3 p-2 rounded-md text-xl text-white">{label}</button>;
 };
 
 export default Button;
